bfa9c78e2d56d95b24062cec047b503869c9380b
Shallman Quashie <shallmanq@gmail.com>
2025-08-21 03:58:36 +0100

    convex package

---
+++ packages/backend/convex/users.ts
@@ -0,0 +1,22 @@
+import { v } from "convex/values";
+import { query, mutation } from "./_generated/server";
+
+export const getMany = query({
+  args: {},
+  handler: async ctx => {
+    const users = await ctx.db.query("users").collect();
+
+    return users;
+  }
+});
+
+export const add = mutation({
+  args: {},
+  handler: async ctx => {
+    const userId = await ctx.db.insert("users", {
+      name: "Basbasa"
+    });
+
+    return userId;
+  }
+});
