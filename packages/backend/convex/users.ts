1aee707cacf27726f479cf0c3bee37c58f284510
Shallman Quashie <shallmanq@gmail.com>
2025-08-22 21:29:02 +0100

    Implement clerk authentication

--- packages/backend/convex/users.ts
+++ packages/backend/convex/users.ts
@@ -13,6 +13,12 @@ export const getMany = query({
 export const add = mutation({
   args: {},
   handler: async ctx => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (identity === null) {
+      throw new Error("Not authenticated");
+    }
+
     const userId = await ctx.db.insert("users", {
       name: "Basbasa"
     });
